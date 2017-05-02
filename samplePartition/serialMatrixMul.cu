abd05480d9f2a1bb38c072394f614dfe3a265c1b
Dominic Grande <dominic.c.grande@gmail.com>
2017-05-02 16:32:19 -0500

    Indexing problems

--- samplePartition/serialMatrixMul.cu
+++ samplePartition/serialMatrixMul.cu
@@ -10,7 +10,7 @@ void serialMatrixMultiply(float *A, float *B, float *C,
         for(int j=0; j<numBColumns; ++j){
             C[i*numBColumns+j]=0;
             for(int k=0; k<numAColumns; ++k)
-                C[i*numBColumns+j]=C[i*numBColumns+j]+(A[i*numAColumns+k]*B[k*numBColumns+j]);
+                C[i*numBColumns+j]=C[i*numBColumns+j]+(A[(i-numAStart)*numAColumns+k]*B[k*numBColumns+j]);
         }
     }
 }
