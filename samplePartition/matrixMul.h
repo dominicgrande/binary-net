41cf587ac11bb9583bad4443c1388aa6eea26331
Dominic Grande <dominic.c.grande@gmail.com>
2017-05-02 03:30:51 -0500

    Fixed header file

--- samplePartition/matrixMul.h
+++ samplePartition/matrixMul.h
@@ -3,6 +3,6 @@
 __global__ void matrixMultiplyShared(float *A, float *B, float *C,
                                      int numARows, int numAColumns,
                                      int numBRows, int numBColumns,
-                                     int numCRows, int numCColumns) 
+                                     int numCRows, int numCColumns); 
 
 #endif
