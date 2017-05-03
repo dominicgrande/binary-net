b1d067217584d9e52b431b5a1e4b0b381c2108a6
Dominic Grande <dominic.c.grande@gmail.com>
2017-05-02 22:29:37 -0500

    Add serial matrix code

--- cpu_gpu_gemm/kernel.cpp
+++ cpu_gpu_gemm/kernel.cpp
@@ -66,3 +66,21 @@ void run_cpu_threads(T *matrix_out, T *matrix, std::atomic_int *flags, int n, in
     }
     std::for_each(cpu_threads.begin(), cpu_threads.end(), [](std::thread &t) { t.join(); });
 }
+
+
+void serialMatrixMultiply(float *A, float *B, float *C,
+                                     int numARows, int numAColumns,
+                                     int numBRows, int numBColumns,
+                                     int numCRows, int numCColumns,
+                                     int numAStart){
+
+                                         //m is row first matrix, n is column first matrix
+                                         //p is row second matrix, q is column second matrix
+    for(int i=numAStart; i<numARows; ++i){
+        for(int j=0; j<numBColumns; ++j){
+            C[i*numBColumns+j]=0;
+            for(int k=0; k<numAColumns; ++k)
+                C[i*numBColumns+j]=C[i*numBColumns+j]+(A[(i-numAStart)*numAColumns+k]*B[k*numBColumns+j]);
+        }
+    }
+}
