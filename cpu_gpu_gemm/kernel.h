28876389a459f44c2508ffbc3322ef2b45c08345
Dominic Grande <dominic.c.grande@gmail.com>
2017-05-02 22:29:19 -0500

    Add serial matrix multiplication

--- cpu_gpu_gemm/kernel.h
+++ cpu_gpu_gemm/kernel.h
@@ -11,14 +11,13 @@ void run_cpu_threads(T *matrix_out, T *matrix, std::atomic_int *flags, int n, in
 #endif
     );
 
-cudaError_t call_Padding_kernel(int blocks, int threads, int n, int m, int pad, int n_tasks, float alpha, 
-    T *matrix_out, T *matrix, int *flags
-#ifdef CUDA_8_0
-    , int l_mem_size, int *worklist
-#endif
-		);
-
 cudaError_t call_GPU_Kernel(int blocks, int threads, int numAColumns, int numARows, int numBColumns, int numBRows
-     float alpha, int numCRows, int numCColumns, float *weights, float *x, float* output)
+     float alpha, int numCRows, int numCColumns, float *weights, float *x, float* output);
+
+void serialMatrixMultiply(float *A, float *B, float *C,
+                                     int numARows, int numAColumns,
+                                     int numBRows, int numBColumns,
+                                     int numCRows, int numCColumns,
+                                     int numAStart);
 
 #endif KERNEL_H
