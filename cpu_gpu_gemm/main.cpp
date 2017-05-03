9aeec91d63e44514e4dd506f0655e1ef2a12486a
Dominic Grande <dominic.c.grande@gmail.com>
2017-05-03 00:51:38 -0500

    Added some protection for alpha

--- cpu_gpu_gemm/main.cpp
+++ cpu_gpu_gemm/main.cpp
@@ -57,17 +57,19 @@ void CPU_GPU_Gemm(float * A, float * B, float * C, float alpha,
                                  C_Row, C_Column, B, A, C);
     printf("Made it after GPU kernel. Need sync\n");
     float* temp_A_Host;
-    temp_A_Host = (float *)malloc(sizeof(float)*A_Row*A_Column*(1-alpha));
+    if (alpha<1){
+        temp_A_Host = (float *)malloc(sizeof(float)*A_Row*A_Column*(1-alpha));
 
-    cudaMemcpy(temp_A_Host, &A[A_GPU_Row], sizeof(float)*(1-alpha)*A_Row*A_Column, cudaMemcpyDeviceToHost);
+        cudaMemcpy(temp_A_Host, &A[A_GPU_Row], sizeof(float)*(1-alpha)*A_Row*A_Column, cudaMemcpyDeviceToHost);
 
-    printf("Memcpy is no good.\n");
+        printf("Memcpy is no good.\n");
 
-    serialMatrixMultiply(temp_A_Host, B_Host, C_Host, 
-                        A_Row, A_Column,
-                        B_Row, B_Column,
-                        C_Row, C_Column,
-                        A_GPU_Row, A_Row);
+        serialMatrixMultiply(temp_A_Host, B_Host, C_Host, 
+                            A_Row, A_Column,
+                            B_Row, B_Column,
+                            C_Row, C_Column,
+                            A_GPU_Row, A_Row);
+    }
     
 
     // Launch CPU threads
