ef4a9e1d775f49dd534508023e58ca0c1168441e
Dominic Grande <dominic.c.grande@gmail.com>
2017-05-02 22:30:52 -0500

    Moved function to correct location

--- cpu_gpu_gemm/kernel.cpp
+++ cpu_gpu_gemm/kernel.cpp
@@ -68,19 +68,3 @@ void run_cpu_threads(T *matrix_out, T *matrix, std::atomic_int *flags, int n, in
 }
 
 
-void serialMatrixMultiply(float *A, float *B, float *C,
-                                     int numARows, int numAColumns,
-                                     int numBRows, int numBColumns,
-                                     int numCRows, int numCColumns,
-                                     int numAStart){
-
-                                         //m is row first matrix, n is column first matrix
-                                         //p is row second matrix, q is column second matrix
-    for(int i=numAStart; i<numARows; ++i){
-        for(int j=0; j<numBColumns; ++j){
-            C[i*numBColumns+j]=0;
-            for(int k=0; k<numAColumns; ++k)
-                C[i*numBColumns+j]=C[i*numBColumns+j]+(A[(i-numAStart)*numAColumns+k]*B[k*numBColumns+j]);
-        }
-    }
-}
