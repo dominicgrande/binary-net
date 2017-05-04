b862f2c6f4884b597fa63c4b9fd4877f9ce260dd
Dominic Grande <dominic.c.grande@gmail.com>
2017-05-04 05:08:32 -0500

    Merge main with John's pull

--- cpu_gem_xnor/main.cpp
+++ cpu_gem_xnor/main.cpp
@@ -12,79 +12,6 @@
 #include <thread>
 #include <assert.h>
 
-
-unsigned int multiply_and_pop(unsigned int A, unsigned int B){
-    unsigned int x_or_value = A ^ B;
-    unsigned int x = __builtin_popcount(x_or_value);
-    std::cout << "Bit count value: " << x << std::endl;
-    return x;
-}
-
-void multiplyMatrices(unsigned int* A, unsigned int* B, float* C, 
-int rowFirst, int columnFirst, int rowSecond, int columnSecond)
-{
-	int i, j, k;
-	// Multiplying matrix firstMatrix and secondMatrix and storing in array mult.
-	for(i = 0; i < rowFirst; ++i)
-	{
-		for(j = 0; j < columnSecond; ++j)
-		{
-			for(k=0; k<columnFirst; ++k)
-			{
-				C[i*columnSecond+j] += multiply_and_pop(A[i*columnFirst+k], B[k*columnSecond+j]);
-			}
-            C[i*columnSecond+j] = -(2*(float)C[i*columnSecond+j]-32*columnFirst);
-		}
-	}
-}
-
-unsigned int concatenate(float* array)
-{
-    unsigned int rvalue=0;
-    unsigned int sign;
-    
-    for (int i = 0; i < 32; i++)
-    {
-        sign = (array[i]>=0);
-        rvalue = rvalue | (sign<<i);
-    }
-    
-    return rvalue;
-}
-
-void concatenate_rows_serial(float* input, unsigned int* output, 
-                                int row, int column){
-    for (int i=0; i<(row*column)/32; i++){
-        output[i] = concatenate(&input[i*32]);
-    }   
-
-    std::cout <<  "The value of the output array for rows is: " << std::endl;
-    for (int i=0; i<32; i++)
-        std::cout <<std::setbase(16) << std::showbase <<(int)output[i] << " " ;
-
-    std::cout << std::endl;
-}
-
-
-void concatenate_cols_serial(float* input, unsigned int* output, 
-                                int m, int n){
-    
-    for (int j=0; j<n; j++){
-        float * array = new float[32];
-        for(int i=0; i<m; i+=32){
-            for(int k=0; k<32;k++) 
-                array[k] = input[j + n*(i+k)];
-            output[j+n*i/32]=concatenate(array); 
-        } 
-        delete[] array;
-    }
-     std::cout <<  "The value of the output array for cols is: " << std::endl;
-    for (int i=0; i<32; i++)
-        std::cout <<std::setbase(16) << std::showbase <<(int)output[i] << " ";
-    
-    std::cout << std::endl;
-}
-
 void CPU_GPU_Gemm(float * A, float * B, float * C, float alpha,
                   int A_Row, int A_Column,
                   int B_Row, int B_Column,
@@ -124,12 +51,6 @@ void CPU_GPU_Gemm(float * A, float * B, float * C, float alpha,
 
         cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, 
                 A_CPU_Row, B_Column, A_Column, 1, temp_A_Host, A_Column, B_Host, B_Column, 0.0, C_Host, B_Column);
-
-        // serialMatrixMultiply(temp_A_Host, B_Host, C_Host, 
-                            // A_Row, A_Column,
-                            // B_Row, B_Column,
-                            // C_Row, C_Column,
-                            // A_GPU_Row, A_Row);
         free(temp_A_Host);
 
     }
@@ -206,9 +127,7 @@ int main(){
 
     C_Row = A_Row;
     C_Column = B_Column;
-    float alpha = .0101;
-    // float alpha = .0000;
-    // float alpha = 0.0001;
+    float alpha = 0.8;
 
     A = (float *)malloc(A_Row*A_Column*sizeof(float));
     B = (float *)malloc(B_Row*B_Column*sizeof(float));
