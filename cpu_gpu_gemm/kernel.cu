270e54a2be2f14ac676ccd69823c1d519d169ef4
Dominic Grande <dominic.c.grande@gmail.com>
2017-05-12 17:10:37 -0500

    Use matthieu old gemm kernel

--- cpu_gpu_gemm/kernel.cu
+++ cpu_gpu_gemm/kernel.cu
@@ -80,59 +80,116 @@ __global__ void gemm(float* A, float* B, float* C, int m, int n, int k) {
 
 }
 */
-__global__ void gemm(float* A, float* B, float* C, int m, int n, int k){
+// __global__ void gemm(float* A, float* B, float* C, int m, int n, int k){
 
-	int numARows = n;
-	int numAColumns = m;
-	int numBRows = n;
-	int numBColumns = k;
-	int numCRows = m;
-	int numCColumns = k;
+// 	int numARows = n;
+// 	int numAColumns = m;
+// 	int numBRows = n;
+// 	int numBColumns = k;
+// 	int numCRows = m;
+// 	int numCColumns = k;
 
 
-  __shared__ float sm[K][TILE_WIDTH_N];
+//   __shared__ float sm[K][TILE_WIDTH_N];
 
   
-  int tx = (threadIdx.x%TILE_WIDTH_N); 
-  int ty = (threadIdx.x/TILE_WIDTH_N);
-  int bx = blockIdx.x;  int by = blockIdx.y;
-  int Col = bx * TILE_WIDTH_M; 
-  int Row = by * TILE_WIDTH_N; 
+//   int tx = (threadIdx.x%TILE_WIDTH_N); 
+//   int ty = (threadIdx.x/TILE_WIDTH_N);
+//   int bx = blockIdx.x;  int by = blockIdx.y;
+//   int Col = bx * TILE_WIDTH_M; 
+//   int Row = by * TILE_WIDTH_N; 
   
-  float reg[K] = {0.0};
-  float temp[TILE_WIDTH_N] = {0.0};
+//   float reg[K] = {0.0};
+//   float temp[TILE_WIDTH_N] = {0.0};
   
 
-  for(int i=0; i<ceil(numBRows/(float)K); ++i)
-  {
+//   for(int i=0; i<ceil(numBRows/(float)K); ++i)
+//   {
 
-     for(int arridx = 0; arridx<K; ++arridx)
-     {
-        if(Col+threadIdx.x<numAColumns && (i*K+arridx) < numARows)
-          reg[arridx] = A[(i*K*numAColumns)+(numAColumns*arridx)+Col+threadIdx.x];
-        else
-          reg[arridx] = 0.0;
-     }
+//      for(int arridx = 0; arridx<K; ++arridx)
+//      {
+//         if(Col+threadIdx.x<numAColumns && (i*K+arridx) < numARows)
+//           reg[arridx] = A[(i*K*numAColumns)+(numAColumns*arridx)+Col+threadIdx.x];
+//         else
+//           reg[arridx] = 0.0;
+//      }
     
-     if(Row+tx<numBColumns && (i*K+ty) < numBRows  )
-        sm[ty][tx] = B[(i*K+ty)*numBColumns+ Row+tx];
-     else
-        sm[ty][tx] = 0.0;
-    __syncthreads();
+//      if(Row+tx<numBColumns && (i*K+ty) < numBRows  )
+//         sm[ty][tx] = B[(i*K+ty)*numBColumns+ Row+tx];
+//      else
+//         sm[ty][tx] = 0.0;
+//     __syncthreads();
 
-    for(int r1=0; r1 < K; ++r1)
-    {
-      for(int c1=0; c1<TILE_WIDTH_N; ++c1)
-        temp[c1] += reg[r1] * sm[r1][c1];
-    }
-      __syncthreads();
-  }
+//     for(int r1=0; r1 < K; ++r1)
+//     {
+//       for(int c1=0; c1<TILE_WIDTH_N; ++c1)
+//         temp[c1] += reg[r1] * sm[r1][c1];
+//     }
+//       __syncthreads();
+//   }
             
-  for(int c1=0; c1<TILE_WIDTH_N; ++c1)
-  {
-      if(Row+c1 < numCColumns&&Col+threadIdx.x <numCRows )
-        C[(Col+threadIdx.x)*numCColumns+Row+c1] = temp[c1];
-  }
+//   for(int c1=0; c1<TILE_WIDTH_N; ++c1)
+//   {
+//       if(Row+c1 < numCColumns&&Col+threadIdx.x <numCRows )
+//         C[(Col+threadIdx.x)*numCColumns+Row+c1] = temp[c1];
+//   }
+// }
+
+__global__ void gemm(float* A, float* B, float* C, int m, int n, int k) {
+
+    // Block row and column
+    int blockRow = blockIdx.y;
+    int blockCol = blockIdx.x;
+    
+    // Thread row and column within Csub
+    int row = threadIdx.y;
+    int col = threadIdx.x;
+
+    // Each thread block computes one sub-matrix Csub of C
+    float* Csub = &C[BLOCK_SIZE * k * blockRow + BLOCK_SIZE * blockCol];
+
+    // Shared memory used to store Asub and Bsub respectively
+    __shared__ float As[BLOCK_SIZE][BLOCK_SIZE];
+    __shared__ float Bs[BLOCK_SIZE][BLOCK_SIZE];
+    
+    // Each thread computes one element of Csub
+    // by accumulating results into Cvalue
+    // block_size = 16 -> 256 threads, one per Csub element
+    float Cvalue = 0.0;
+    
+    // Loop over all the sub-matrices of A and B that are
+    // required to compute Csub
+    // Multiply each pair of sub-matrices together
+    // and accumulate the results
+    for (int i = 0; i < (n / BLOCK_SIZE); ++i) {
+    
+        // Get sub-matrix Asub of A
+        float* Asub = &A[BLOCK_SIZE * blockRow * n + BLOCK_SIZE * i];
+        
+        // Get sub-matrix Bsub of B
+        float* Bsub = &B[BLOCK_SIZE * k * i + BLOCK_SIZE * blockCol];
+        
+        // Load Asub and Bsub from device memory to shared memory
+        // Each thread loads one element of each sub-matrix
+        As[row][col] = Asub[row*n+col];
+        Bs[row][col] = Bsub[row*k+col];
+    
+        // Synchronize to make sure the sub-matrices are loaded
+        // before starting the computation
+        __syncthreads();
+        
+        // Multiply Asub and Bsub together
+        for (int j = 0; j < BLOCK_SIZE; ++j) Cvalue += As[row][j] * Bs[j][col]; 
+        
+        // Synchronize to make sure that the preceding
+        // computation is done before loading two new
+        // sub-matrices of A and B in the next iteration
+        __syncthreads();
+    }
+    
+    // Write Csub to device memory
+    // Each thread writes one element
+    if(col + blockCol* BLOCK_SIZE< k && row + blockRow* BLOCK_SIZE< m) Csub[row*k+col] = Cvalue;
 }
 
 __global__ void transpose(float* A, float* B, int m, int n)
