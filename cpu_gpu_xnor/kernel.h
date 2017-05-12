79a71e375832f59774670815a6ba612eccb8de74
John Lee <yoonlee95@gmail.com>
2017-05-11 21:23:24 -0500

    shared library for cpu_gem_xnor made

---
+++ cpu_gpu_xnor/kernel.h
@@ -0,0 +1,19 @@
+#ifndef KERNEL_H
+#define KERNEL_H
+
+#include "cuda_runtime.h"
+#include <atomic>
+#include "support/common.h"
+
+// void run_cpu_threads(T *matrix_out, T *matrix, std::atomic_int *flags, int n, int m, int pad, int num_threads, int ldim, int n_tasks, float alpha
+// #ifdef CUDA_8_0
+//     , std::atomic_int *worklist
+// #endif
+//     );
+
+unsigned int concatenate(float* array);
+void call_GPU_concatenate_rows(int n, int m, float* A, unsigned int* Ac, cudaStream_t kernel_stream);
+void call_GPU_concatenate_cols(int n, int m, int k, float* B, unsigned int* Bc, cudaStream_t kernel_stream);
+void call_GPU_xnor(int n, int m, int k, unsigned int* Ac, unsigned int* Bc, float* C, cudaStream_t kernel_stream);
+
+#endif 
